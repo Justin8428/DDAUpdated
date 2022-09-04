2725bf5d80e66023b51a5a7dc33756dcb8879a06
justin <justin@JUSTIN11-PC>
2022-09-05 00:47:23 +1000

    attempt at detecting video playback -- currently broken

--- DDA/SettingsForm.cs
+++ DDA/SettingsForm.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Drawing;
 using System.Management;
 using System.Management.Automation;
@@ -147,27 +148,66 @@ public class SettingsForm : Form
 
 	private bool IsPlayingVideo() // determine if the display needs to be kept awake, e.g. playing a video
 	{   // also see https://stackoverflow.com/questions/206323/how-to-execute-command-line-in-c-get-std-out-results 
-        // and https://stackoverflow.com/questions/33654318/c-sharp-run-powershell-command-get-output-as-it-arrives
-        // underlying: https://docs.microsoft.com/en-us/windows/win32/power/system-sleep-criteria
-        // essentially, run the powershell script to see if DISPLAY is active, return true if so, otherwise return false
-  //      PowerShell ps = PowerShell.Create();
-		//string script = "(powercfg /requests | Select-String -Pattern 'DISPLAY:' -Context 0,1).Context.DisplayPostContext"; // should return 'None.' if display not used
-		//ps.AddScript(script);
+		// and https://stackoverflow.com/questions/33654318/c-sharp-run-powershell-command-get-output-as-it-arrives
+		// underlying: https://docs.microsoft.com/en-us/windows/win32/power/system-sleep-criteria
+		// essentially, run the powershell script to see if DISPLAY is active, return true if so, otherwise return false
+		// todo: create a custom runspace for better memory usage https://docs.microsoft.com/en-us/powershell/scripting/developer/hosting/windows-powershell-host-quickstart?view=powershell-7.2, or use code below
+		// apparently for powercfg /requests to run properly you need to compile the exe to the particular platform you are running...!
 
-  //      // invoke execution on the pipeline (collecting output)
-  //      Collection<PSObject> PSOutput = ps.Invoke();
+		PowerShell ps = PowerShell.Create();
+		// string script = "(powercfg /requests | Select-String -Pattern 'DISPLAY:' -Context 0,1).Context.DisplayPostContext | Out-String"; // should return 'None.' if display not used
+		string script = "powercfg /requests";
+		ps.AddScript(script);
 
-  //      // loop through each output object item
-  //      foreach (PSObject outputItem in PSOutput)
-  //      {
-  //          // if null object was dumped to the pipeline during the script then a null object may be present here
-  //          if (outputItem != null)
-  //          {
-  //              System.Diagnostics.Debug.WriteLine($"Output line: [{outputItem}]"); // view outputs
-  //          }
-  //      }
+		// invoke execution on the pipeline (collecting output)
+		Collection<PSObject> PSOutput = ps.Invoke();
 
-		return false; } 
+		// define result variable to prevent code path issues
+		bool resultVar = false; // ignore the video monitoring as failsafe
+
+		// loop through each output object item
+		foreach (PSObject outputItem in PSOutput)
+		{
+			// if null object was dumped to the pipeline during the script then a null object may be present here
+			if (outputItem != null)
+			{
+				System.Diagnostics.Debug.WriteLine($"Output line: [{outputItem}]"); // view outputs
+				string outputString = outputItem.BaseObject as string; // coerce into string https://stackoverflow.com/questions/37467404/how-to-cast-psobject-to-securestring-cannot-implicitly-convert
+				outputString = outputString.Trim('\r', '\n'); // strip new line characters
+				string noVideoPlaying = "None."; // response from powercfg if there is no video playing
+				string noElevation = "This command requires administrator privileges and must be executed from an elevated command prompt."; // response from powercfg if not run as admin
+				if (String.Equals(outputString, noVideoPlaying) || String.Equals(outputString, noElevation)) // if there is no hook, or powercfg is not run as admin, assume no video is playing (and dim screen)
+				{
+					resultVar = false;
+				}
+				else
+				{
+					resultVar = true; // the loop iterates over each line of the object. if video is playing, last line will be "Video Wake Lock" which will give rise to the final resultVar
+				}
+			}
+			else
+			{
+				resultVar = false; // ignore the video monitoring as failsafe
+			}
+		}
+		return resultVar;
+
+
+		// taken from https://stackoverflow.com/questions/14981785/calling-powercfg-requests-from-c-sharp-gives-wrong-values
+		// maybe refactor into this code, it looks way cleaner and maybe more memory efficient because all the parsing will be done in C# instead of powershell
+		//Process p = new Process();
+		//p.StartInfo.UseShellExecute = false;
+		//p.StartInfo.RedirectStandardOutput = true;
+		//p.StartInfo.FileName = "powercfg";
+		//p.StartInfo.Arguments = "/requests";
+		//p.Start();
+
+		//string output = p.StandardOutput.ReadToEnd();
+		//p.WaitForExit();
+		//System.Diagnostics.Debug.WriteLine($"Output line: [{output}]");
+		//return false;
+
+	}
 
 	// actual logic to set and unset brightness after given time
 	private void TimerEventProcessor(object myObject, EventArgs myEventArgs)
@@ -175,7 +215,7 @@ public class SettingsForm : Form
 		TimeSpan idleTime = GetIdleTime();
 		if ((Settings.Default.IdleDelay == 0 && idleTime.TotalSeconds >= 10.0) || (Settings.Default.IdleDelay >= 1 && idleTime.TotalMinutes >= (double)Settings.Default.IdleDelay))
 		{
-			bool isPlayingVideo = IsPlayingVideo(); // currently unimplemented
+			bool isPlayingVideo = IsPlayingVideo(); 
 			if (!isDimmed && !isPlayingVideo) // if display is not dimmed and not playing video
 			{
 				restoreTo = GetBrightness();
